be3724cee674d0d2e0a4e269d2d74094483ac0cc
Shingo Kitagawa <shingogo@hotmail.co.jp>
2021-08-20 19:43:57 +0900

    restart/reload/start gdrive systemd

--- gdrive_recorder/scripts/reload_all_services.bash
+++ gdrive_recorder/scripts/reload_all_services.bash
@@ -4,9 +4,15 @@ set -x
 sudo systemctl stop jsk-pr1040-gdrive-recorder.service
 sudo systemctl stop jsk-fetch15-gdrive-recorder.service
 sudo systemctl stop jsk-fetch1075-gdrive-recorder.service
+sudo systemctl stop jsk-pr1040-gdrive.service
+sudo systemctl stop jsk-fetch15-gdrive.service
+sudo systemctl stop jsk-fetch1075-gdrive.service
 
 sudo systemctl daemon-reload
 
+sudo systemctl start jsk-pr1040-gdrive.service
+sudo systemctl start jsk-fetch15-gdrive.service
+sudo systemctl start jsk-fetch1075-gdrive.service
 sudo systemctl start jsk-pr1040-gdrive-recorder.service
 sudo systemctl start jsk-fetch15-gdrive-recorder.service
 sudo systemctl start jsk-fetch1075-gdrive-recorder.service
