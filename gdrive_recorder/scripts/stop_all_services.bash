c516251f5367a5a73503aae3c0b9c3f4007c875e
Shingo Kitagawa <shingogo@hotmail.co.jp>
2022-02-17 13:37:39 +0900

    add stop bash script

---
+++ gdrive_recorder/scripts/stop_all_services.bash
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+set -x
+sudo systemctl stop jsk-pr1040-gdrive.service
+sudo systemctl stop jsk-fetch15-gdrive.service
+sudo systemctl stop jsk-fetch1075-gdrive.service
+sudo systemctl stop jsk-pr1040-gdrive-recorder.service
+sudo systemctl stop jsk-fetch15-gdrive-recorder.service
+sudo systemctl stop jsk-fetch1075-gdrive-recorder.service
+set +x
