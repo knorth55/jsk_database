1441d05f180c8aee4f9136579419e9fb21561057
Shingo Kitagawa <shingogo@hotmail.co.jp>
2023-04-02 16:04:17 +0900

    support server upload

--- gdrive_recorder/node_scripts/gdrive_recorder.py
+++ gdrive_recorder/node_scripts/gdrive_recorder.py
@@ -3,8 +3,10 @@
 from datetime import datetime
 import influxdb
 import os
+import os.path
 import pytz
 import requests
+import shutils
 import signal
 import subprocess
 import sys
@@ -75,10 +77,18 @@ class GdriveRecorder(object):
 
         if self.upload_dest == 'gdrive':
             self.upload_service = rospy.Service(
-                '~upload', Trigger, self._upload_service_cb)
+                '~upload', Trigger, self._upload_gdrive_service_cb)
             self.upload_timer = rospy.Timer(
                 rospy.Duration(self.upload_duration),
-                self._upload_timer_cb)
+                self._upload_gdrive_timer_cb)
+        else:
+            self.upload_server_dir = '/mnt/{}'.format(self.upload_dest)
+            self.upload_service = rospy.Service(
+                '~upload', Trigger,
+                self._upload_server_service_cb)
+            self.upload_timer = rospy.Timer(
+                rospy.Duration(self.upload_duration),
+                self._upload_server_timer_cb)
 
         if self.video_topic_name:
             self.video_sub = rospy.Subscriber(
@@ -115,11 +125,20 @@ class GdriveRecorder(object):
         if self.video_topic_name:
             self.video_subscribed = False
 
-    def _upload_timer_cb(self, event):
-        self._upload()
+    def _upload_gdrive_timer_cb(self, event):
+        self._upload_gdrive()
 
-    def _upload_service_cb(self, req):
-        result = self._upload()
+    def _upload_gdriver_service_cb(self, req):
+        self._upload_service_cb(self._upload_gdrive)
+
+    def _upload_server_timer_cb(self, event):
+        self._upload_server()
+
+    def _upload_server_service_cb(self, req):
+        self._upload_service_cb(self._upload_server)
+
+    def _upload_service_cb(self, upload_func):
+        result = upload_func()
         success = True
         message = ''
         fail_count = 0
@@ -128,8 +147,8 @@ class GdriveRecorder(object):
             message = 'No file uploaded'
             return TriggerResponse(success=success, message=message)
 
-        for res in result:
-            for suc in res.successes:
+        for success in result:
+            for suc in success:
                 if not suc:
                     success = False
                     fail_count = fail_count + 1
@@ -137,19 +156,35 @@ class GdriveRecorder(object):
             message = 'Failed to upload {} files'.format(fail_count)
         return TriggerResponse(success=success, message=message)
 
-    def _upload(self):
-        rospy.loginfo('start uploading')
+    def _remove_zero_file(self, file_path):
+        file_size = os.path.getsize(file_path)
+        # check if file size is not empty
+        if file_size == 0:
+            try:
+                os.remove(file_path)
+            except OSError:
+                pass
+            rospy.logwarn('Filesize is 0: {}'.format(file_path))
+            return True
+        else:
+            return False
+
+    def _create_file_titles(self):
         file_titles = os.listdir(self.video_path)
         file_titles = [
             x for x in file_titles if x.endswith(
                 '_{}_record_video.avi'.format(self.robot_name))]
+
+    def _create_file_paths(self, file_titles):
         if self.video_title in file_titles:
             file_titles.remove(self.video_title)
         if len(file_titles) == 0:
             rospy.loginfo('No file found to upload')
             return None
         file_paths = ['{}/{}'.format(self.video_path, x) for x in file_titles]
+        return file_paths
 
+    def _create_file_days_dict(self, file_titles):
         file_days_dict = {}
         for file_id, file_title in enumerate(file_titles):
             file_day = file_title.split('_')[0]
@@ -157,6 +192,78 @@ class GdriveRecorder(object):
                 file_days_dict[file_day].append(file_id)
             else:
                 file_days_dict[file_day] = [file_id]
+        return file_days_dict
+
+    def _upload_gdrive_step(
+            self, file_day, upload_file_paths, upload_file_titles):
+        upload_parents_path = self.upload_parents_path + '/' + file_day
+        req = MultipleUploadRequest()
+        req.file_paths = upload_file_paths
+        req.file_titles = upload_file_titles
+        req.parents_path = upload_parents_path
+        req.use_timestamp_folder = False
+        req.use_timestamp_file_title = False
+        gdrive_upload = rospy.ServiceProxy(
+            '/{}/upload_multi'.format(self.gdrive_server_name),
+            MultipleUpload
+        )
+        res = gdrive_upload(req)
+
+        # influxdb
+        if self.store_url:
+            query = []
+            for success, file_url, file_path, file_title, in zip(
+                    res.successes, res.file_urls,
+                    upload_file_paths, upload_file_titles):
+                if success:
+                    rospy.loginfo('Upload succeeded: {}'.format(file_path))
+                    stamp = '_'.join(file_title.split('_')[:2])
+                    stamp = self.localtz.localize(
+                        datetime.strptime(stamp, '%Y%m%d_%H%M%S%Z'))
+                    stamp = stamp.astimezone(self.tz).isoformat('T')
+                    query.append({
+                        "measurement": "gdrive_recorder",
+                        "time": stamp,
+                        "fields": {
+                            "file_title": file_title,
+                            "file_path": file_path,
+                            "file_url": file_url,
+                        }
+                    })
+                else:
+                    rospy.loginfo('Upload failed: {}'.format(file_path))
+            if len(query) > 0:
+                self.client.write_points(query, time_precision='ms')
+        return res.successes
+
+    def _upload_server_step(
+            self, file_day, upload_file_paths, upload_file_titles):
+        success = []
+        if not os.path.ismount(self.upload_server_dir):
+            rospy.logerr('{} is not mounted.'.format(self.upload_server_dir)
+            return success
+
+        server_file_dir = '{}/{}'.format(self.upload_server_dir, file_day)
+        if not os.path.exists(server_file_dir):
+            os.path.makedirs(server_file_dir)
+
+        for (upload_file_path, upload_tile_title) in zip(
+                upload_file_paths, upload_file_titles):
+            server_file_path = '{}/{}'.format(server_file_dir, server_file_path)
+            try:
+                rospy.loginfo('copy: {} -> {}'.format(upload_file_path, server_file_path))
+                shutils.copy(upload_file_path, server_file_path)
+                success.append(True)
+            except Exception as e:
+                rospy.logerr(e)
+                success.append(False)
+        return success
+
+    def _upload(self, _upload_step_func):
+        rospy.loginfo('start uploading')
+        file_titles = self._create_file_titles()
+        file_paths = self._create_file_paths(file_titles)
+        file_days_dict = self._create_file_days_dict(file_titles)
 
         result = []
         for file_day in sorted(file_days_dict.keys()):
@@ -164,72 +271,34 @@ class GdriveRecorder(object):
             upload_file_ids = []
             for file_id in file_ids:
                 file_path = file_paths[file_id]
-                file_size = os.path.getsize(file_path)
-                # check if file size is not empty
-                if file_size == 0:
-                    try:
-                        os.remove(file_path)
-                    except OSError:
-                        pass
-                    rospy.logwarn('Filesize is 0: {}'.format(file_path))
-                else:
+                if not self._remove_zero_file(file_path):
                     upload_file_ids.append(file_id)
 
             if len(upload_file_ids) == 0:
                 continue
 
-            upload_parents_path = self.upload_parents_path + '/' + file_day
             upload_file_paths = [
                 x for i, x in enumerate(file_paths) if i in upload_file_ids]
             upload_file_titles = [
                 x for i, x in enumerate(file_titles) if i in upload_file_ids]
-            req = MultipleUploadRequest()
-            req.file_paths = upload_file_paths
-            req.file_titles = upload_file_titles
-            req.parents_path = upload_parents_path
-            req.use_timestamp_folder = False
-            req.use_timestamp_file_title = False
-            gdrive_upload = rospy.ServiceProxy(
-                '/{}/upload_multi'.format(self.gdrive_server_name),
-                MultipleUpload
-            )
-            res = gdrive_upload(req)
-            result.append(res)
+            success = _upload_step_func(
+                upload_parents_path, upload_file_paths, upload_file_titles)
+            result.append(success)
 
-            # influxdb
-            if self.store_url:
-                query = []
-                for success, file_url, file_path, file_title, in zip(
-                        res.successes, res.file_urls,
-                        upload_file_paths, upload_file_titles):
-                    if success:
-                        rospy.loginfo('Upload succeeded: {}'.format(file_path))
-                        stamp = '_'.join(file_title.split('_')[:2])
-                        stamp = self.localtz.localize(
-                            datetime.strptime(stamp, '%Y%m%d_%H%M%S%Z'))
-                        stamp = stamp.astimezone(self.tz).isoformat('T')
-                        query.append({
-                            "measurement": "gdrive_recorder",
-                            "time": stamp,
-                            "fields": {
-                                "file_title": file_title,
-                                "file_path": file_path,
-                                "file_url": file_url,
-                            }
-                        })
-                    else:
-                        rospy.loginfo('Upload failed: {}'.format(file_path))
-                if len(query) > 0:
-                    self.client.write_points(query, time_precision='ms')
-
-            for success, file_path in zip(res.successes,  upload_file_paths):
-                if success:
+            for suc, file_path in zip(success,  upload_file_paths):
+                if suc:
                     try:
                         os.remove(file_path)
                     except OSError:
                         pass
         rospy.loginfo('stop uploading')
         return result
+
+    def _upload_gdrive(self):
+        return self._upload(self._upload_gdrive_step)
+
+    def _upload_server(self):
+        return self._upload(self._upload_server_step)
 
     def _record_timer_cb(self, event):
         self.start_time = rospy.Time.now()
